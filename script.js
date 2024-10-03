f23fe108b7f33027f08a01bc6a557267ba87dcde
VictorLander22 <victordlander@hotmail.com>
2024-10-03 17:17:22 -0300

    teste

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const refugoChart = new Chart(ctxRefugo, {
         datasets: [{
             label: 'Refugo',
             data: [12, 19, 3, 5, 2],
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            backgroundColor: 'rgba(0, 0, 255, 1)',
             borderColor: 'rgba(255, 99, 132, 1)',
             borderWidth: 1
         }]
